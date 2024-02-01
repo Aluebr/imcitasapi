76459f79f9fb741ed08feff7d4a082ee6fc587f2
Jose Vicente Ebri Gomez <aluebr5363@ieselcaminas.org>
2024-02-01 02:16:02 +0100

    Fix some CitrasController Issues

--- src/main/java/com/cji/citas/dto/CitasDTO.java
+++ src/main/java/com/cji/citas/dto/CitasDTO.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 @Data
 public class CitasDTO {
     private Long id;
-    private LocalDateTime fecha;
     private LocalDateTime horaInicio;
     private LocalDateTime horaFin;
     private UsersDTO usuario;
