4d9517af9c2d62d61feb97a1f841d80936050990
jvebri <ebriigomezjv@gmail.com>
2024-02-05 19:04:37 +0100

    Import SQL

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-
+INSERT INTO users (email, name,password,roles) values ('Carlos','carlosch@ieselcaminas.org','$2a$10$XFaSbq8.TSC5TlFNop8y3.hikxHdXw6M4L/Mtk4CFaiqIxHbwsUPS',('ROLE_USER,ROLE_ADMIN'))
 
